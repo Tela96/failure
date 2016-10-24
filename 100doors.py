8add29de3879d277096ede494c486e70f38b1722
Telakos <tel.akos.2@gmail.com>
2016-10-24 12:01:24 +0200

    added my utter failure... It does nothing, but it does it good

---
+++ 100doors.py
@@ -0,0 +1,32 @@
+#all doors closed
+#all doors open
+#every 2nd door is toggled
+#every 3rd door is toggled
+
+#2 lists - open and closed doors
+counter = 1
+n = 0
+doors = []
+cdoors = []
+state= ()
+while counter <= 100:
+    state = 0
+    doors.append(state)
+    counter = (counter+1)
+counter = 2
+while n <= 100
+
+
+counter = counter+n
+if state = 0:
+
+
+
+
+
+
+
+
+
+ 
+
